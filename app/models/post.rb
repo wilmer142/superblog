d8b1b1b53fea86a92d93e46ffa01597a206e13ab
wilmer142 <wilmercollege@gmail.com>
2019-02-03 16:14:31 -0500

    Add Post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :title, :body, presence: {message: "%{value} no puede estar en blanco"}
+  validates :body, length: {minimum: 250, message: "%{value} es demasiado corto (250 caracteres mínimo)"}
 end
