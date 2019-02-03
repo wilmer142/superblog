72d8375f972271b33ade7aa47ee62e96c0e7fb4b
wilmer142 <wilmercollege@gmail.com>
2019-02-03 16:42:46 -0500

    Add index method, filters and private methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,10 @@
 class PostsController < ApplicationController
+
+  before_action :authenticate_user!, :except => [:show, :index]
+  before_action :find_post, :except => [:index, :new, :create]
+
   def index
+    @posts = Post.all
   end
 
   def show
@@ -19,4 +24,14 @@ class PostsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+    def find_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :body, :user_id)
+    end
 end
