03a608e5a719895468d7c7f494cdcb8b7b59203f
wilmer142 <wilmercollege@gmail.com>
2019-02-03 19:14:47 -0500

    Add root to post#index and resource post routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/edit'
-  get 'posts/update'
-  get 'posts/destroy'
+  
+  root 'posts#index'
+
+  resources :posts
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
